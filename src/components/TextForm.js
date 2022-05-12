05170c5808f7a256d0068d1543a6a3167a0818ad
Subham Maity <97989643+Subham-Maity@users.noreply.github.com>
2022-05-13 02:45:38 +0530

    Fixing few more Textutils Issues

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -72,11 +72,11 @@ export default function TextForm(props) {
         props.showAlert("Remove duplicates" , "success");
     }
     const handleCopy = () => {
-        console.log("I am copy");
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        // console.log("I am copy");
+        // var text = document.getElementById("myBox");
+        // text.select();
+        navigator.clipboard.writeText(text);
+        // document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success");
     }
     const handleClearClick = ()=>{
